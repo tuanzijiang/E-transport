29d09a0133c455a82a40e10f63feca9abafca475
sumAlbert <982675088@qq.com>
2017-05-20 17:33:09 +0800

    some change by zq

--- app/src/main/java/com/example/dell2/e_transport/MainActivity.java
+++ app/src/main/java/com/example/dell2/e_transport/MainActivity.java
@@ -8,8 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
     }
 
 }
