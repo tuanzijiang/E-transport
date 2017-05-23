26bcb588b388d7d0c58bf43ae1647149701ff616
wangyan <wang077@qq.com>
2017-05-23 14:37:15 +0800

    code by wy

---
+++ app/src/main/java/com/example/dell2/e_transport/SettingActivity.java
@@ -0,0 +1,30 @@
+package com.example.dell2.e_transport;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import collector.BaseActivity;
+
+/**
+ * Created by wangyan on 2017/5/23.
+ */
+
+public class SettingActivity extends BaseActivity{
+    private ImageView header_front_1;
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
+        setContentView(R.layout.activity_setting);
+        init();
+    }
+
+    public void init(){
+        header_front_1=(ImageView) findViewById(R.id.header_front_1);
+        header_front_1.setImageResource(R.drawable.last_white);
+    }
+    
+}
