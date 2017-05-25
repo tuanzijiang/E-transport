73dddf1eaff643ded6ec52d7f544454b7b224315
sumAlbert <982675088@qq.com>
2017-05-25 12:48:23 +0800

    some change by zq

--- app/src/main/java/com/example/dell2/e_transport/AccountActivity.java
+++ app/src/main/java/com/example/dell2/e_transport/AccountActivity.java
@@ -23,8 +23,14 @@ public class AccountActivity extends BaseActivity implements View.OnClickListene
     private TextView userName;
     private LinearLayout setUserGender;
     private TextView userGender;
-    private LinearLayout setTel;
-    private TextView tel;
+    private LinearLayout setUserTel;
+    private TextView userTel;
+    private LinearLayout setUserAddress;
+    private TextView userAddress;
+    private LinearLayout setPwLogin;
+    private TextView pwLogin;
+    private LinearLayout setPwCover;
+    private TextView pwCover;
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -40,28 +46,39 @@ public class AccountActivity extends BaseActivity implements View.OnClickListene
         header_back_1=(ImageView)findViewById(R.id.header_back_1);
         title_name=(TextView)findViewById(R.id.title_name);
         setUserName=(LinearLayout)findViewById(R.id.setUserName);
-        userName=(TextView)findViewById(R.id.userName);
+        setUserAddress=(LinearLayout)findViewById(R.id.setUserAddress);
+        setUserTel=(LinearLayout)findViewById(R.id.setTel);
         setUserGender=(LinearLayout)findViewById(R.id.setUserGender);
+        setPwCover=(LinearLayout)findViewById(R.id.set_PwCover);
+        setPwLogin=(LinearLayout)findViewById(R.id.set_PwLogin);
+        userName=(TextView)findViewById(R.id.userName);
         userGender=(TextView)findViewById(R.id.userGender);
-        setTel=(LinearLayout)findViewById(R.id.setTel);
-        tel=(TextView)findViewById(R.id.tel);
-        /*相应事件*/
+        userTel=(TextView)findViewById(R.id.tel);
+        userAddress=(TextView)findViewById(R.id.userAddress);
+        pwLogin=(TextView)findViewById(R.id.pwLogin);
+        pwCover=(TextView)findViewById(R.id.pwCover);
+        /*响应事件设定*/
         setUserName.setOnClickListener(this);
         header_front_1.setImageResource(R.drawable.last_white);
         header_back_1.setVisibility(View.GONE);
         title_name.setText("账户信息");
-
     }
+    /*响应事件*/
     public void onClick(View view){
         Intent intent;
         switch (view.getId()){
+            case R.id.header_back_1:
+                finish();
+                break;
             case R.id.setUserName:
                 intent=new Intent(AccountActivity.this,UserNameActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.setUserSex:
+            case R.id.setUserGender:
                 intent=new Intent(AccountActivity.this,UserSexActivity.class);
                 startActivity(intent);
+            case R.id.setTel:
+//                intent=new Intent(AccountActivity.this)
             default:
                 break;
         }
