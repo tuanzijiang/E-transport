3b6e96e9ec51ab005033656160f81a913a7a025b
zjclyzs <704847159@qq.com>
2017-05-31 23:09:49 +0800

    添加有关网络连接的异步消息类以及接口，在BaseActivity中加入相关的处理函数

--- app/src/main/java/collector/BaseActivity.java
+++ app/src/main/java/collector/BaseActivity.java
@@ -1,8 +1,11 @@
 package collector;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import collector.AppCompatActivityCollector;
 /**
@@ -19,4 +22,29 @@ public class BaseActivity extends AppCompatActivity {
         super.onDestroy();
         AppCompatActivityCollector.removeActivity(this);
     }
+    protected void sendHttpPostRequest(String url, CommonRequest request, ResponseHandler responseHandler, boolean showLoadingDialog) {
+        new HttpPostTask(request, mHandler, responseHandler).execute(url);
+    /*    if(showLoadingDialog) {
+            LoadingDialogUtil.showLoadingDialog(BaseActivity.this);
+        }*/
+    }
+
+    protected Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(msg.what == Constant.HANDLER_HTTP_SEND_FAIL) {
+                Log.d("SEND_FAIL",msg.obj.toString());
+
+                //LoadingDialogUtil.cancelLoading();
+                Toast.makeText(BaseActivity.this, "请求发送失败，请重试", Toast.LENGTH_SHORT).show();
+            } else if (msg.what == Constant.HANDLER_HTTP_RECEIVE_FAIL) {
+                Log.d("RECEIVE_FAIL",msg.obj.toString());
+
+                //LoadingDialogUtil.cancelLoading();
+                Toast.makeText(BaseActivity.this, "请求接受失败，请重试", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
---
+++ app/src/main/java/collector/Constant.java
@@ -0,0 +1,23 @@
+package collector;
+
+/**
+ * Created by fanmiaomiao on 2017/5/31.
+ */
+
+public class Constant {
+
+    public static final int HANDLER_HTTP_SEND_FAIL = 1001;
+    public static final int HANDLER_HTTP_RECEIVE_FAIL = 1002;
+
+    /*
+     * Activity跳转用RequestCode
+     */
+    public static int REQUEST_CODE_ = 0;
+
+    /*
+     * Activity跳转用ResponseCode
+     */
+    public static int RESPONSE_CODE_SUCCESS = 100;
+    public static int RESPONSE_CODE_FAIL = 101;
+    public static int RESPONSE_CODE_NO_RESULT = 102;
+}
