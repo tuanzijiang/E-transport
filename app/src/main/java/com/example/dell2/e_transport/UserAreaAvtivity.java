164495ae96035dcd34a3637f85ab8e46e6d9e37d
hualp <823227806@qq.com>
2017-06-08 14:32:55 +0800

    BY hualp config only

--- app/src/main/java/com/example/dell2/e_transport/UserAreaAvtivity.java
+++ app/src/main/java/com/example/dell2/e_transport/UserAreaAvtivity.java
@@ -5,6 +5,7 @@ import collector.BaseActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,16 +14,52 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.navi.AMapNaviViewListener;
+
 /**
  * Created by dell2 on 2017/5/25.
  */
 
-public class UserAreaAvtivity extends BaseActivity implements View.OnClickListener {
+public class UserAreaAvtivity extends BaseActivity implements View.OnClickListener,AMapLocationListener{
+    MapView mMapView = null;
+//    private TextView textView;
+//    private String[] strMsg;
+//    //声明AMapLocationClient类对象
+//    public AMapLocationClient mLocationClient = null;
+//    //声明定位回调监听器
+//    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+//        @Override
+//        public void onLocationChanged(AMapLocation loc) {
+//            if (null!=loc){
+//                Message msg=mHandler.obtainMessage();
+//                msg.obj=loc;
+//                ms
+//            }
+//        }
+//
+//
+//    };
+////初始化定位
+//    mLocationClient = new AMapLocationClient(getApplicationContext());
+////设置定位回调监听
+//    mLocationClient.setLocationListener(mLocationListener);
+//
+//
+
+
+
+    //old*******************
     private ImageView back_button;
     private E_Trans_Application app;
     private ImageView search_button;
     private EditText location;
     private RelativeLayout button_verify;
+    private AMap aMap;
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -30,9 +67,43 @@ public class UserAreaAvtivity extends BaseActivity implements View.OnClickListen
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_usermap);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(saveInstanceState);
+
+
+//        textView=(TextView)findViewById(R.id.location);
+//        Location();
         init();
     }
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+//    private void Location() {
+//    }
+protected void onResume() {
+    super.onResume();
+    //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+    mMapView.onResume();
+}
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
     private void init(){
+
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
         /*实例化*/
         button_verify=(RelativeLayout)findViewById(R.id.button_verify);
         back_button=(ImageView)findViewById(R.id.back_button);
@@ -100,5 +171,10 @@ public class UserAreaAvtivity extends BaseActivity implements View.OnClickListen
      *  初始化地图中心点的位置
      */
     public void initLocation(){
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+
     }
 }
