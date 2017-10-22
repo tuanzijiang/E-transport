8e139fc050882203418921471625d1d7f163afc0
zjclyzs <704847159@qq.com>
2017-10-23 00:55:23 +0800

    重修

--- app/src/main/java/collector/Constant.java
+++ app/src/main/java/collector/Constant.java
@@ -8,12 +8,12 @@ public class Constant {
 
     public static final int HANDLER_HTTP_SEND_FAIL = 1001;
     public static final int HANDLER_HTTP_RECEIVE_FAIL = 1002;
-    public static final String SETTING_URL = "http://118.89.191.184:8080/ETServer/ETHome/SettingServlet";
-    public static final String LOGIN_URL = "http://118.89.191.184:8080/ETServer/ETHome/LoginServlet";
-    public static final String ADDRESS_URL = "http://118.89.191.184:8080/ETServer/ETHome/AddressServlet";
-    public static final String REQUEST_ADDRESS_URL = "http://118.89.191.184:8080/ETServer/ETHome/RequestAddress";
-    public static final String ORDER_URL = "http://118.89.191.184:8080/ETServer/ETHome/OrderServlet";
-    public static final String REQUEST_ORDER_URL = "http://118.89.191.184:8080/ETServer/ETHome/RequestOrder";
+    public static final String SETTING_URL = "http://182.254.220.56:8080/ETServer/ETHome/SettingServlet";
+    public static final String LOGIN_URL = "http://182.254.220.56:8080/ETServer/ETHome/LoginServlet";
+    public static final String ADDRESS_URL = "http://182.254.220.56:8080/ETServer/ETHome/AddressServlet";
+    public static final String REQUEST_ADDRESS_URL = "http://182.254.220.56:8080/ETServer/ETHome/RequestAddress";
+    public static final String ORDER_URL = "http://182.254.220.56:8080/ETServer/ETHome/OrderServlet";
+    public static final String REQUEST_ORDER_URL = "http://182.254.220.56:8080/ETServer/ETHome/RequestOrder";
 
     public static String avatarPath = null;
 }
